1f4d628ea8918e6f8e27672380e45086c369b3ac
Alexander L Myers <am411@pvhs-lab112-d04.pvsd.org>
2016-09-07 14:20:45 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("What's going on?");
+        System.out.print("Hello World");
+       // The above is the 2 ways we need
+        // to know how to output text
+        // 2 types of comments
+        // 1. Line comment and that is done with a //
+        // 2. Block comment. That is done with a /*
+        System.out.print(1);
+        System.out.print(1.0);
+    }
+}
